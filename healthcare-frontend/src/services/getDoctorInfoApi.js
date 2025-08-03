626ab626e1032efbf6f937b58dc14e73fd93717d
Venkata Reddy <reddyvenkat1995@gmail.com>
2025-08-03 22:05:59 +0530

    updated urls

--- healthcare-frontend/src/services/getDoctorInfoApi.js
+++ healthcare-frontend/src/services/getDoctorInfoApi.js
@@ -1,6 +1,6 @@
 const getDoctorInfo = async (doctorId) =>{
     //console.log(doctorId);
-    const response = await fetch(`https://healthcare-appointments-app.onrender.com/${doctorId}`)
+    const response = await fetch(`https://healthcare-appointments-app.onrender.com/fetch-doctor-info/${doctorId}`)
     const data = await response.json();
     //console.log(data);
     return data;
